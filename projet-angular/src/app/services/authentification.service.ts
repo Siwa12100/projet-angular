a106f1e09a341aefc84cdf341a61bdd43bd6d0d2
Siwa12100 <jean.marcillac12@gmail.com>
2024-10-18 09:07:40 +0000

    travail sur service d'authentification !

--- projet-angular/src/app/services/authentification.service.ts
+++ projet-angular/src/app/services/authentification.service.ts
@@ -1,29 +1,45 @@
 import { Injectable } from '@angular/core';
-import { RedirectCommand, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { LocalStorageService } from './local-storage.service';
+import { JoueursService } from './joueurs.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
 
-  constructor(protected router: Router, protected localStorage : LocalStorageService) { }
+  constructor(
+    protected router: Router,
+    protected localStorage: LocalStorageService,
+    protected joueursService: JoueursService
+  ) {}
 
-  onInit(){
-    if(!this.localStorage.getData('username')){
-        this.router.navigate(['/login'])
-    }
-    else{
-        this.router.navigate(['/'])
+  onInit() {
+    if (!this.localStorage.getData('username')) {
+      this.router.navigate(['/login']);
+    } else {
+      this.router.navigate(['/']);
     }
   }
 
-  login(login : string, password: string) : boolean {
-    if(login == password){
-        this.localStorage.saveData('username', login);
-        this.router.navigate(['/'])
-        return true;
+  login(login: string, password: string): boolean {
+    if (login !== password) {
+      console.log("mdp et login different....");
+      return false;
     }
-    return false;
+
+    this.joueursService.recupererLogins().subscribe(loginsExistants => {
+      const loginExiste = loginsExistants.includes(login); 
+      if (!loginExiste) {
+        console.log("le login existe pas....");
+        return false;
+      }
+
+      this.localStorage.saveData('username', login);
+      this.router.navigate(['/']);
+      return true;
+    });
+
+    return true;
   }
 }
