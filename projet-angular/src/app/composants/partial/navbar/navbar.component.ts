d6ae768e08ce9a50e999139996ef202ca3a726c6
Siwa12100 <jean.marcillac12@gmail.com>
2024-10-18 17:10:07 +0000

    taff sur la navbar

--- projet-angular/src/app/composants/partial/navbar/navbar.component.ts
+++ projet-angular/src/app/composants/partial/navbar/navbar.component.ts
@@ -1,33 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavbarMenuComponent } from "./menu/menu.component";
 import { LocalStorageService } from '../../../services/local-storage.service';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthentificationService } from '../../../services/authentification.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-navbar',
-    standalone: true,
-    imports: [NavbarMenuComponent],
-    templateUrl: './navbar.component.html', 
-    styleUrl : './navbar.component.css'
-  })
-  export class NavbarComponent implements OnInit {
-    pseudo : string | null;
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [NavbarMenuComponent],
+  templateUrl: './navbar.component.html', 
+  styleUrl : './navbar.component.css'
+})
+export class NavbarComponent implements OnInit, OnDestroy {
+  pseudo: string | null = null;
+  private routerSubscription: Subscription | null = null;
 
-    constructor(
-      protected router : Router,
-      protected authentificationService : AuthentificationService) {
-
-      this.pseudo = this.authentificationService.recupererPseudoJoueurConnecte();
-    }
-
-    ngOnInit(): void {
-        this.pseudo = this.authentificationService.recupererPseudoJoueurConnecte();
-    }
-
-    deconnexion() : void {
-      this.authentificationService.logout();
-      this.pseudo = null;
-    }
-    
+  constructor(
+    protected router: Router,
+    protected authentificationService: AuthentificationService
+  ) {
+    this.pseudo = this.authentificationService.recupererPseudoJoueurConnecte();
   }
+
+  ngOnInit(): void {
+    this.pseudo = this.authentificationService.recupererPseudoJoueurConnecte();
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.rafraichirComposant();
+      }
+    });
+  }
+
+  deconnexion(): void {
+    this.authentificationService.logout();
+    this.pseudo = null;
+  }
+
+  rafraichirComposant(): void {
+    this.pseudo = this.authentificationService.recupererPseudoJoueurConnecte();
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+}
